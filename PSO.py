db10e055f707e5f80f4cf7fb4e6a0c5693d3eaec
Jailson Liberato <jailsonliberato@MacBook-Pro-de-Jailson.local>
2019-04-09 01:32:35 -0300

    Criação do Sphere (faltando o FocalBest e gráficos) e Rastring.

--- PSO.py
+++ PSO.py
@@ -1,19 +1,64 @@
 import pyswarms as ps
+from pyswarms.utils.functions import single_obj as fx
+import numpy as np
+
 
 class PSOClass:
 
-    options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+    optionsGlobal = {'c1': 2.05, 'c2': 2.05, 'w':0.8}
+    optionsLocal = {'c1': 2.05, 'c2': 2.05, 'w':0.8, 'k': 2, "p": 2}
 
     def __init__(self):
-        self.executarSphere()
+        #self.executarSphere()
         self.executarRastrigin()
         self.executarRosenbrock()
 
     def executarSphere(self):
-        print("EXECUTANDO SPHERE")
+        print("-> EXECUTANDO SPHERE")
+        print("[Global Best]")
+        optimizer = ps.single.GlobalBestPSO(n_particles=30, dimensions=30, options=self.optionsGlobal)
+        cost, pos = optimizer.optimize(fx.sphere, iters=10000)
+        print("COST Global")
+        print(cost)
+        print("COST pos")
+        print(pos)
 
+        print("[Local Best]")
+        optimizer = ps.single.LocalBestPSO(n_particles=30, dimensions=30, options=self.optionsLocal)
+        cost, pos = optimizer.optimize(fx.sphere, iters=10000)
+        print("COST Global")
+        print(cost)
+        print("COST pos")
+        print(pos)
+
+    """ print([Focal Best])
+        optimizer = ps.single.FocalBestPSO(n_particles=30, dimensions=30, options=self.options)
+        cost, pos = optimizer.optimize(fx.sphere, iters=10000)
+        print("COST Global")
+        print(cost)
+        print("COST pos")
+        print(pos)
+        """
     def executarRastrigin(self):
-        print("EXECUTANDO RASTRIGIN")
+        max_bound = 5.12 * np.ones(2)
+        min_bound = - max_bound
+        bounds = (min_bound, max_bound)
+        options = {'c1': 2.05, 'c2': 2.05, 'w':0.8}
+
+        # Call instance of PSO with bounds argument
+        optimizer = ps.single.GlobalBestPSO(n_particles=30, dimensions=2, options=options, bounds=bounds)
+
+        # Perform optimization
+        cost, pos = optimizer.optimize(fx.rastrigin, iters=10000)
+
+
+        #print("[Local Best]")
+        #optimizer = ps.single.LocalBestPSO(n_particles=30, dimensions=30, options=self.optionsLocal)
+        #cost, pos = optimizer.optimize(fx.rastrigin, iters=10000)
+        #print("COST Global")
+        #print(cost)
+        #print("COST pos")
+        #print(pos)
 
     def executarRosenbrock(self):
         print("EXECUTANDO ROSENBROCK")
