f80af4dd743b957ed9dcb9da25da75f72385e404
Jailson Liberato <jailsonliberato@MacBook-Pro-de-Jailson.local>
2019-04-10 20:18:19 -0300

    Criação do Menu para escolha das funções.

--- Main.py
+++ Main.py
@@ -1,11 +1,18 @@
 """ Main class of execution."""
 class Main:
     def executar(self):
-        print("\n\n")
-        print("#"*40)
-        print("\tPARTICLE SWARM OPTIMIZATION")
-        print("#"*40)
-        print("\n\n")
+        option = ' '
+        while option != '0':
+            print("\n\n")
+            print("#"*40)
+            print("\tPARTICLE SWARM OPTIMIZATION")
+            print("#"*40)
+            print("\n\n")
+            print("[1] - PSO using Sphere.")
+            print("[2] - PSO using Rastringin.")
+            print("[3] - PSO using Rosenbrock.")
+            print("[0] - Exit")
+            option = input("Choose your option: ")
 
 main = Main()
 main.executar()
