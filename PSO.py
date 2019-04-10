b1fc8a3aa2799b82d9f3be26f0083ecc4f0aadde
Jailson Liberato <jailsonliberato@MacBook-Pro-de-Jailson.local>
2019-04-10 01:20:51 -0300

    Colocação dos gráficos e das legendas.

--- PSO.py
+++ PSO.py
@@ -1,7 +1,8 @@
 import pyswarms as ps
 from pyswarms.utils.functions import single_obj as fx
 import numpy as np
-
+from pyswarms.utils.plotters import plot_cost_history
+import matplotlib.pyplot as plt
 
 class PSOClass:
 
@@ -11,33 +12,32 @@ class PSOClass:
     def __init__(self):
         #self.executarSphere()
         self.executarRastrigin()
-        self.executarRosenbrock()
+        #self.executarRosenbrock()
 
     def executarSphere(self):
         print("-> EXECUTANDO SPHERE")
         print("[Global Best]")
         optimizer = ps.single.GlobalBestPSO(n_particles=30, dimensions=30, options=self.optionsGlobal)
-        cost, pos = optimizer.optimize(fx.sphere, iters=10000)
-        print("COST Global")
-        print(cost)
-        print("COST pos")
-        print(pos)
+        cost, posGlobalBest = optimizer.optimize(fx.sphere, iters=10000)
+        print(posGlobalBest)
 
         print("[Local Best]")
         optimizer = ps.single.LocalBestPSO(n_particles=30, dimensions=30, options=self.optionsLocal)
-        cost, pos = optimizer.optimize(fx.sphere, iters=10000)
-        print("COST Global")
-        print(cost)
-        print("COST pos")
-        print(pos)
+        cost, posLocalBest = optimizer.optimize(fx.sphere, iters=10000)
+        print(posLocalBest)
+
+        #plt.scatter(posGlobalBest, posLocalBest)
+        plt.figure("Function Sphere")
+        plt.plot(posGlobalBest,'-g', label='GPSO')
+        plt.plot(posLocalBest, '-o', label='LPSO')
+        plt.legend(loc='upper left')
+        plt.ylabel('Fitness')
+        plt.title("Function Sphere")
+        plt.show()
 
     """ print([Focal Best])
         optimizer = ps.single.FocalBestPSO(n_particles=30, dimensions=30, options=self.options)
         cost, pos = optimizer.optimize(fx.sphere, iters=10000)
-        print("COST Global")
-        print(cost)
-        print("COST pos")
-        print(pos)
         """
     def executarRastrigin(self):
         max_bound = 5.12 * np.ones(2)
@@ -49,16 +49,19 @@ class PSOClass:
         optimizer = ps.single.GlobalBestPSO(n_particles=30, dimensions=2, options=options, bounds=bounds)
 
         # Perform optimization
-        cost, pos = optimizer.optimize(fx.rastrigin, iters=10000)
+        cost, posGlobalBest = optimizer.optimize(fx.rastrigin, iters=10000)
 
+       # print("[Local Best]")
+       # optimizer = ps.single.LocalBestPSO(n_particles=30, dimensions=2, options=self.optionsLocal)
+        #cost, posLocalBest = optimizer.optimize(fx.rastrigin, iters=10000)
 
-        #print("[Local Best]")
-        #optimizer = ps.single.LocalBestPSO(n_particles=30, dimensions=30, options=self.optionsLocal)
-        #cost, pos = optimizer.optimize(fx.rastrigin, iters=10000)
-        #print("COST Global")
-        #print(cost)
-        #print("COST pos")
-        #print(pos)
+        plt.figure("Function Rastrigin")
+        plt.plot(posGlobalBest,'-g', label='GPSO')
+        #plt.plot(posLocalBest, '-o', label='LPSO')
+        plt.legend(loc='upper left')
+        plt.ylabel('Fitness')
+        plt.title("Function Rastrigin")
+        plt.show()
 
     def executarRosenbrock(self):
         print("EXECUTANDO ROSENBROCK")
