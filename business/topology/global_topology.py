ad4bfb813f45ec13e60cd19c52fc75b41c07e1f4
jailsonliberato <jailsonliberato@gmail.com>
2019-05-09 01:02:12 -0300

    Velocidade da topologia global.

--- business/topology/global_topology.py
+++ business/topology/global_topology.py
@@ -1,7 +1,15 @@
 from business.topology.topology import Topology
+from business.functions.fitness_function import FitnessFunction
+from util.constants import Constants
+import random
 
 
 class GlobalTopology(Topology):
 
-    def calculate_velocity(self):
-        pass
+    def calculate_velocity(self, particles, gbest, fitness_function: FitnessFunction, inertia: float):
+        for particle in particles:
+            r1 = random.uniform(0, 1)
+            r2 = random.uniform(0, 1)
+            particle.velocity = inertia * particle.velocity + Constants.COEFFICIENT1 * r1 * \
+                                (particle.pbest - particle.position) + Constants.COEFFICIENT2 * r2 \
+                                * (gbest - particle.position)
